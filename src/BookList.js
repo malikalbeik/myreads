4b1de96191b3ee755e41d31809daca7cd5a9eb3f
Malik Albeik <abd.beik@me.com>
2018-03-11 23:53:03 +0300

    Added some text to display when there are no books on the shelf.

--- src/BookList.js
+++ src/BookList.js
@@ -10,46 +10,53 @@ class BookList extends Component {
         </div>
         <div className="list-books-content">
           {this.props.shelfs.map(shelf => {
-            var shelfsBooks = this.props.books.filter(book => book.shelf === shelf.value)
-            return (
-              <div className="bookshelf" key={shelf.value}>
-                <h2 className="bookshelf-title">{shelf.name}</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {shelfsBooks.map((book) => {
-                      return (
-                        <li key={book.id}>
-                          <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover"
-                                   style={{ width: 128,
-                                            height: 193,
-                                            backgroundImage: "url(" + book.imageLinks.thumbnail + ")"
-                                          }}></div>
-                              <div className="book-shelf-changer">
-                                <select value={book.shelf} onChange={event => this.props.whenShelfChanges(book.id, event)}>
-                                  <option value="none" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
+            var shelfsBooks = this.props.books.filter(book => book.shelf === shelf.value);
+            if (shelfsBooks.length !== 0) {
+              return (
+                <div className="bookshelf" key={shelf.value}>
+                  <h2 className="bookshelf-title">{shelf.name}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {shelfsBooks.map((book) => {
+                        return (
+                          <li key={book.id}>
+                            <div className="book">
+                              <div className="book-top">
+                                <div className="book-cover" style={{ width: 128,height: 193,backgroundImage: "url(" + book.imageLinks.thumbnail + ")"}}></div>
+                                <div className="book-shelf-changer">
+                                  <select
+                                    value={book.shelf}
+                                    onChange={event => this.props.whenShelfChanges(book, event)}>
+
+                                    <option value="none" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                  </select>
+                                </div>
                               </div>
+                              <div className="book-title">{book.title}</div>
+                              <div className="book-authors">{book.authors}</div>
                             </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
-                          </div>
-                        </li>
-
-                      )}
-                  )}
-              </ol>
-            </div>
-          </div>
-        )}
-
-
-          )}
+                          </li>
+                        )
+                      })}
+                    </ol>
+                  </div>
+                </div>
+              )
+            } else {
+              return(
+                <div className="bookshelf" key={shelf.value}>
+                  <h2 className="bookshelf-title">{shelf.name}</h2>
+                  <div className="bookshelf-books">
+                    <p>You have no books on this shelf, Please <Link to='/addbook'>Add</Link> some</p>
+                  </div>
+                </div>
+              )
+            }
+          })}
         </div>
         <div className="open-search">
           <Link to='/addbook'>Add a book</Link>
