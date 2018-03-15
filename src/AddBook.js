7ee9d97905f0f43f101da46d485cf141e6d856cd
Malik Albeik <abd.beik@me.com>
2018-03-15 14:15:36 +0300

    imported important components and added debounce to the search input so that onchange happens once every 100 milliseconds

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import { Debounce } from "react-throttle";
+import PropTypes from "prop-types";
+import sortBy from 'sort-by'
 
 class AddBook extends Component {
   render() {
@@ -8,15 +12,24 @@ class AddBook extends Component {
         <div className="search-books-bar">
           <Link to='/' className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <Debounce time="100" handler="onChange">
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                onChange={(event) => {this.getBooks(event.target.value)} }
+                />
+            </Debounce>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {booksList}
+          </ol>
         </div>
       </div>
     )
   }
+
 }
 
 export default AddBook
