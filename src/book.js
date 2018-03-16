1613e7061a2fe94473141fd5a815b02eacea40c5
Malik Albeik <abd.beik@me.com>
2018-03-16 17:37:02 +0300

    refactored the book's code to be its own component

--- src/book.js
+++ src/book.js
@@ -2,9 +2,38 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Book extends Component {
-  render() {
-      return (
 
+  static propTypes = {
+  book: PropTypes.object.isRequired,
+  onShelfChange: PropTypes.func.isRequired
+}
+
+  render() {
+    const {book, onShelfChange} = this.props
+      return (
+        <li key={book.id}>
+          <div className='book'>
+            <div className='book-top'>
+              <div className='book-cover' style={width: 128,height: 193,backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : noThumbLink})`}/>
+
+              <div className='book-shelf-changer'>
+                <select value={book.shelf} onChange={event => {
+                    event.preventDefault();
+                    this.props.onShelfChange(book, event.target.value);
+                  }}>
+                  <option value='none' disabled>Move to...</option>
+                  <option value='currentlyReading'>Currently Reading</option>
+                  <option value='wantToRead'>Want to Read</option>
+                  <option value='read'>Read</option>
+                  <option value='none'>None</option>
+                </select>
+              </div>
+
+            </div>
+            <div className='book-title'>{book.title}</div>
+            <div className='book-authors'>{book.authors}</div>
+          </div>
+        </li>
       )
     }
   }
